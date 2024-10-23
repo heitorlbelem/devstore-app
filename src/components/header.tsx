d56b0cb447e18a74d98954bf0baf0d4a578b6097
Heitor Belém <heitorlbelem98@gmail.com>
2024-10-22 23:23:32 -0300

    Using cart context inside server components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
-import { Search, ShoppingBag } from 'lucide-react'
+import { Search } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { CartWidget } from './cart-widget'
 
 export function Header() {
   return (
@@ -21,13 +22,8 @@ export function Header() {
       </div>
 
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
-          <ShoppingBag className="h-4 w-4" />
-          <span className="text-sm">Cart {0}</span>
-        </div>
-
+        <CartWidget />
         <div className="h-4 w-px bg-zinc-700" />
-
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
           <Image
